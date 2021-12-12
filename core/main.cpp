9ce05b75da35caa45c0177c2a725a9e89ad58013
Yaroslav Oleynik <yaraoley2005@mail.ru>
2021-12-13 00:48:58 +0300

    Added some Cmake configurations and updated main OpenCV loop with exitable conditions of static flag

--- core/main.cpp
+++ core/main.cpp
@@ -2,43 +2,37 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+
+//#include <pybind11/pybind11.h>
 #include <cstdio>
-using namespace cv;
+namespace opencv = cv;
 using namespace std;
+
+
+static bool isAppInRun = true;
+
 int main(int, char**)
 {
-    Mat frame;
-    //--- INITIALIZE VIDEOCAPTURE
-    VideoCapture cap;
-    // open the default camera using default API
-    // cap.open(0);
-    // OR advance usage: select any API backend
-    int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-    // open selected camera using selected API
-    cap.open(deviceID, apiID);
-    // check if we succeeded
-    if (!cap.isOpened()) {
+    opencv::Mat frame;
+    opencv::VideoCapture capturing("./../database/VerySeriousVideoForAnalyze.mp4");
+
+    if (!capturing.isOpened()) {
         cerr << "ERROR! Unable to open camera\n";
         return -1;
     }
-    //--- GRAB AND WRITE LOOP
     cout << "Start grabbing" << endl
          << "Press any key to terminate" << endl;
-    for (;;)
+
+    while (isAppInRun)
     {
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
-        // check if we succeeded
+        capturing.read(frame);
         if (frame.empty()) {
             cerr << "ERROR! blank frame grabbed\n";
             break;
         }
-        // show live and wait for a key with timeout long enough to show images
         imshow("Live", frame);
-        if (waitKey(5) >= 0)
-            break;
+        if (opencv::waitKey(5) >= 0)
+            isAppInRun = false;
     }
-    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }

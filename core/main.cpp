531a853f678f11ee2ee7f454f36fe6c883399480
Yaroslav Oleynik <yaraoley2005@mail.ru>
2021-12-14 21:19:24 +0300

    Finally i set up OpenCv and ready to get started with video preprocessing and more cool stuff like that

--- core/main.cpp
+++ core/main.cpp
@@ -3,7 +3,6 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
-//#include <pybind11/pybind11.h>
 #include <cstdio>
 namespace opencv = cv;
 using namespace std;
@@ -13,13 +12,19 @@ static bool isAppInRun = true;
 
 int main(int, char**)
 {
+    // Sorry i have no idea how 2 initialize adoptive file paths
+    string analyzingVideoFilePath = "D:/ProjectsField/neuralNetworkForEmotionDetection/database/VerySeriousVideoForAnalyze.mp4";
+
     opencv::Mat frame;
-    opencv::VideoCapture capturing("./../database/VerySeriousVideoForAnalyze.mp4");
+    opencv::VideoCapture capturing;
+    int apiIdReference = opencv::CAP_ANY;
+    capturing.open(analyzingVideoFilePath, apiIdReference);
 
     if (!capturing.isOpened()) {
         cerr << "ERROR! Unable to open camera\n";
         return -1;
     }
+
     cout << "Start grabbing" << endl
          << "Press any key to terminate" << endl;
 
@@ -28,11 +33,11 @@ int main(int, char**)
         capturing.read(frame);
         if (frame.empty()) {
             cerr << "ERROR! blank frame grabbed\n";
-            break;
+            isAppInRun = false;
         }
-        imshow("Live", frame);
         if (opencv::waitKey(5) >= 0)
             isAppInRun = false;
+        opencv::imshow("Live", frame);
     }
     return 0;
 }

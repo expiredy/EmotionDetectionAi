9d0e685d1bc763e38e5fce57fadbe75a0adda80a
Yaroslav Oleynik <yaraoley2005@mail.ru>
2022-01-05 16:22:41 +0300

    Starting exploring sockets and other cool stuff

--- backend/core/main.cpp
+++ backend/core/main.cpp
@@ -1,43 +1,7 @@
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
+#include <iostream>
 
-//#include "pybind11/pybind11.h"
+int main(int, char**) {
+    
 
-#include <string>
-using std::string;
-namespace opencv = cv;
-
-static bool isAppInRun = true;
-
-opencv::Mat preprocessFrame(opencv::Mat currentFrame){
-    return currentFrame;
-}
-
-
-int main(int, char**)
-{
-    // Sorry i have no idea how 2 initialize adoptive file paths
-    string analyzingVideoFilePath = "D:/ProjectsField/neuralNetworkForEmotionDetection/database/VerySeriousVideoForAnalyze.mp4";
-
-    opencv::Mat frame;
-    opencv::VideoCapture capturing;
-    int apiIdReference = opencv::CAP_ANY;
-    capturing.open(analyzingVideoFilePath, apiIdReference);
-
-    if (!capturing.isOpened()) {
-        return -1;
-    }
-
-    while (isAppInRun)
-    {
-        capturing.read(frame);
-        if (frame.empty()) {
-            isAppInRun = false;
-        }
-        if (opencv::waitKey(5) >= 0)
-            isAppInRun = false;
-        opencv::imshow("Pass", frame);
-    }
     return 0;
 }
--- client_data_input/camera-client/src/components/OptionNavBar.vue
+++ client_data_input/camera-client/src/components/OptionNavBar.vue
@@ -1,17 +1,30 @@
 <template>
-    <div class="option">
+    <div class="option-section">
         <h1>Fuck you!</h1>
+        <HamburgerMenu />
     </div>
 </template>
 
 <script>
+import HamburgerMenu from './HamburgerMenu.vue'
+
 export default {
-  name: 'OptionNavBar',
-  props: {
-  }
+    name: 'OptionNavBar',
+    components: {
+        HamburgerMenu
+    },
+    props: {}
 }
 </script>
 
 <style>
-
+.option-section{
+    padding: 0;
+    border: 0;
+    margin: 0;
+    position: flex;
+    background-color: rgb(0, 129, 235);
+    -webkit-filter: drop-shadow( 25px 25px 7px #000000b3);
+    filter: drop-shadow( 0px 0px 50px rgba(0, 0, 0, .7));
+}
 </style>
